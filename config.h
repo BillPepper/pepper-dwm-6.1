4e563860e4d91712bf7cbd9b631dcbe823c9c782
Mattes Wenzel <mattes@flawlesslogic.net>
2022-06-23 02:21:24 +0200

    upate color scheme to green

--- config.h
+++ config.h
@@ -4,13 +4,14 @@
 static const char *fonts[] = {
 	"monospace:size=10"
 };
+
 static const char dmenufont[]       = "monospace:size=10";
-static const char normbordercolor[] = "#444444";
+static const char normbordercolor[] = "#4f772d";
 static const char normbgcolor[]     = "#222222";
-static const char normfgcolor[]     = "#b2a191";
-static const char selbordercolor[]  = "#222222";
+static const char normfgcolor[]     = "#4f772d";
+static const char selbordercolor[]  = "#90a955";
 static const char selbgcolor[]      = "#222222";
-static const char selfgcolor[]      = "#eeeeee";
+static const char selfgcolor[]      = "#90a955";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
