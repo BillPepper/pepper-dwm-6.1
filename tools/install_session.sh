eb9921059b117d3cee0f282032bf34221cce88f8
Mattes Wenzel <mattes@flawlesslogic.net>
2022-06-23 15:03:50 +0200

    update desktop entry source url

--- tools/install_session.sh
+++ tools/install_session.sh
@@ -1,4 +1,4 @@
-url="https://github.com/benmills/dwm/blob/master/pkg/dwm/usr/share/xsessions/dwm.desktop"
+url="https://raw.githubusercontent.com/benmills/dwm/master/pkg/dwm/usr/share/xsessions/dwm.desktop"
 
 wget $url
 sudo mv dwm.desktop /usr/share/xsessions/
